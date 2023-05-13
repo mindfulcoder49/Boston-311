e9733c6d010993335dcdee9ef529dc6362f68773
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2023-05-13 12:51:18 -0400

    Changes to be committed: 	new file:   manifest.in 	new file:   pyproject.toml 	new file:   setup.py 	renamed:    Boston-311-py-package/__init__.py -> src/__init__.py     renamed:    Boston-311-py-package/data_clean.py -> src/data_clean.py 	renamed:    Boston-311-py-package/load_data.py -> src/load_data.py 	renamed:    Boston-311-py-package/train_models.py -> src/train_models.py 	renamed:    Boston-311-py-package/unit_tests.py -> src/unit_tests.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Boston-311',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'tensorflow',
+        'scikit_learn',
+        'matplotlib'
+    ],
+    entry_points={
+        'console_scripts': [
+            #'my_script_name=my_package_name.my_script_file:main'
+            # Add any other console scripts you want to install here
+        ]
+    },
+    # Metadata about your project
+    author='Alex Alcivar',
+    author_email='alex.g.alcivar49@gmail.com',
+    description='A package for training machine learning models on Boston 311 data',
+    long_description='A package for training machine learning models on Boston 311 data',
+    url='https://github.com/mindfulcoder49/Boston-311',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
