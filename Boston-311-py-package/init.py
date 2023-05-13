0a1d49e2fec7a53d2fc68e6da999d77aa2d81c04
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2023-05-13 11:32:17 -0400

    Changes to be committed: 	new file:   .gitignore 	new file:   Boston-311-py-package/data_clean.py 	new file:   Boston-311-py-package/init.py 	new file:   Boston-311-py-package/load_data.py 	new file:   Boston-311-py-package/train_models.py 	new file:   Boston-311-py-package/unit_tests.py 	modified:   Boston-311.code-workspace 	new file:   results/Boston311.ipynb 	new file:   results/Boston311_v2.ipynb 	new file:   results/Boston311_v3.ipynb 	new file:   results/Boston311_v4.ipynb 	new file:   results/Boston311_v5.ipynb 	new file:   results/Boston311_v6.ipynb 	new file:   results/Boston311_v7.ipynb 	new file:   results/Boston311_v8.ipynb

---
+++ Boston-311-py-package/init.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+import tensorflow as tf
+import glob
+import pprint
+from google.colab import files
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.preprocessing import StandardScaler
+from datetime import datetime
+
+from IPython.display import display
+
+%matplotlib inline
