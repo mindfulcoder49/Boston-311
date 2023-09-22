be7b28ce56255eb7fa04f6ae62d0c4dd8d03a4ad
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2023-09-22 13:09:56 -0400

    Adding KerasNLP

--- src/boston311/__init__.py
+++ src/boston311/__init__.py
@@ -4,3 +4,4 @@ from .Boston311SurvDecTree import Boston311SurvDecTree
 from .Boston311LogReg import Boston311LogReg
 from .Boston311LinReg import Boston311LinReg
 from .Boston311CoxReg import Boston311CoxReg
+from .Boston311KerasNLP import Boston311KerasNLP
